f43ae7147f35e57917fb4daa4b9fbc7d0fa0163c
Vaishnavi Merli <146807774+vaishnavimerli@users.noreply.github.com>
2024-03-14 00:17:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    constmenu =document.querySelector(".menu-links");
+    constmenu=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+
+}
